1722c29b02976ece34edd863cfc7e54079679db6
Albert Shu <albert94shu@gmail.com>
2023-04-24 16:35:28 -0500

    Feature 12 complete

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from projects.models import Project
+from tasks.models import Task
 
 # Register your models here.
 
 admin.site.register(Project)
+admin.site.register(Task)
